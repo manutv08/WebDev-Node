7246f1b362f830695656001bee398a1e94c3388b
Manu V <manutv08@gmail.com>
2022-05-26 09:43:27 +0530

    Ajesh sir class started

---
+++ nodemarch/app.js
@@ -0,0 +1,16 @@
+// console.log("welcome to nodejs")
+// const fs = require('fs')
+// fs.writeFileSync("message.txt","I have edited this file")
+// fs.appendFileSync("message.txt",",I have added.")
+
+const utils = require('./utils')
+// const sname = "John"
+// console.log(utils)
+// const notes = require('./notes')
+console.log(utils(2,3));
+// msg = notes()
+// console.log(msg);
+
+// const validator = require('validator')
+// console.log(validator.isEmail('manumanu.com'));
+// console.log(validator.isURL('https://www.google.com/'));
---
+++ nodemarch/utils.js
@@ -0,0 +1,6 @@
+console.log("this is utils file");
+const sname = "John"
+const add = function(a,b){
+    return a+b
+}
+module.exports = add
